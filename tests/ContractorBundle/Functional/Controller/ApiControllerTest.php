a0ff2dee1f56c846227ecc2c1bb2e5b2dee9ab35
evrinoma <evrinoma@gmail.com>
2021-06-23 13:07:35 +0300

    fix Create tests

--- tests/ContractorBundle/Functional/Controller/ApiControllerTest.php
+++ tests/ContractorBundle/Functional/Controller/ApiControllerTest.php
@@ -2,47 +2,142 @@
 
 namespace Evrinoma\ContractorBundle\Tests\Functional\Controller;
 
+
+use Evrinoma\ContractorBundle\Dto\ContractorApiDto;
+use Symfony\Component\HttpFoundation\Response;
+
 /**
  * @group functional
  */
 class ApiControllerTest extends AbstractControllerTest
 {
+//region SECTION: Fields
+    private array $default = [];
+//endregion Fields
+
+//region SECTION: Protected
+    protected function getDtoClass(): string
+    {
+        return ContractorApiDto::class;
+    }
+
+    protected function getDefault(array $extend): array
+    {
+        return array_merge($extend, unserialize(serialize($this->default)));
+    }
+
+    protected function createContractor(array $query)
+    {
+        $this->client->restart();
+
+        $this->client->request(
+            'POST',
+            'evrinoma/api/contractor/create',
+            [],
+            [],
+            ['CONTENT_TYPE' => 'application/json'],
+            json_encode($query)
+        );
+    }
+
+    protected function randomContractor(): array
+    {
+        return [
+            "name"       => "test company",
+            "id"         => 1,
+            "active"     => "a",
+            "created_at" => "2021-06-08 17:46",
+            "updated_at" => "2021-06-08 17:46",
+
+        ];
+    }
+//endregion Protected
+
 //region SECTION: Public
-    public function testGetAction(): void
+    public function testPostIdentity()
     {
-        $expected  = '{"id":"01623","city":"Lommatzsch"}';
-        $expected2 = '{"id":"01623","city":"Lommatzsch"}';
-        $crawler   = $this->client->request('GET', 'evrinoma/api/contractor?class=Evrinoma%5CContractorBundle%5CDto%5CContractorApiDto&id=3');
+        $this->createIdentity();
 
-        $this->assertEquals($expected, $expected2);
+        $this->assertEquals(Response::HTTP_CREATED, $this->client->getResponse()->getStatusCode());
     }
 
-    public function testCriteriaAction(): void
+    public function testPostIdenityDependency()
     {
-        $this->client->request('GET', '/api/contractor/criteria');
+        $this->createIdentityDependency();
+
+        $this->assertEquals(Response::HTTP_CREATED, $this->client->getResponse()->getStatusCode());
     }
 
-    public function testDeleteAction(): void
+    public function testPostIdenityDependencyIsolate()
     {
-        $this->client->request('DELETE', '/api/contractor/delete');
+        $this->createIdenityDependencyIsolate();
+
+        $this->assertEquals(Response::HTTP_CREATED, $this->client->getResponse()->getStatusCode());
     }
 
-    public function testPutAction(): void
+    public function testPostDublicate()
     {
-        $this->client->request('PUT', '/api/contractor/save');
-    }
+        $this->createIdentity();
+        $this->assertEquals(Response::HTTP_CREATED, $this->client->getResponse()->getStatusCode());
+        $this->createIdentityDependency();
+        $this->assertEquals(Response::HTTP_CREATED, $this->client->getResponse()->getStatusCode());
+        $this->createIdenityDependencyIsolate();
+        $this->assertEquals(Response::HTTP_CREATED, $this->client->getResponse()->getStatusCode());
 
-    public function testPostAction(): void
-    {
-        $this->client->request('POST', '/api/contractor/create');
+        $this->createIdentity();
+        $this->assertEquals(Response::HTTP_CONFLICT, $this->client->getResponse()->getStatusCode());
+        $this->createIdentityDependency();
+        $this->assertEquals(Response::HTTP_CONFLICT, $this->client->getResponse()->getStatusCode());
+        $this->createIdenityDependencyIsolate();
+        $this->assertEquals(Response::HTTP_CONFLICT, $this->client->getResponse()->getStatusCode());
     }
 //endregion Public
+
+//region SECTION: Private
+    private function createIdentity(): void
+    {
+        $query = $this->getDefault(["class" => $this->getDtoClass(), "identity" => "1234567890",]);
+
+        $this->createContractor($query);
+    }
+
+    private function createIdentityDependency(): void
+    {
+        $query = $this->getDefault(["class" => $this->getDtoClass(), "identity" => "1234567890", "dependency" => "1234567890"]);
+
+        $this->createContractor($query);
+    }
+
+    private function createIdenityDependencyIsolate(): void
+    {
+        $query = $this->getDefault(["class" => $this->getDtoClass(),]);
+
+        $this->createContractor($query);
+    }
+//endregion Private
+
+//region SECTION: Dto
+    public function testPostWrongDto()
+    {
+        $this->createWrongDto();
+
+        $this->assertEquals(Response::HTTP_UNPROCESSABLE_ENTITY, $this->client->getResponse()->getStatusCode());
+    }
+
+    private function createWrongDto(): void
+    {
+        $query = $this->getDefault([]);
+
+        $this->createContractor($query);
+    }
+//endregion SECTION: Dto
 
 //region SECTION: Getters/Setters
     public function setUp(): void
     {
+        $this->default = $this->randomContractor();
+
         parent::setUp();
-        //   $this->loadContractorFixtures();
     }
 //endregion Getters/Setters
 }
