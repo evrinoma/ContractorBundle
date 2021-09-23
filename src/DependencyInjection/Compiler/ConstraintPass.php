ae967176cdb27bc213d8f8013531622c8e3fe203
Nikolay Nikolaev <evrinoma@gmail.com>
2021-09-23 17:06:20 +0300

    Compiler Pass logic

--- src/DependencyInjection/Compiler/ConstraintPass.php
+++ src/DependencyInjection/Compiler/ConstraintPass.php
@@ -3,34 +3,19 @@
 namespace Evrinoma\ContractorBundle\DependencyInjection\Compiler;
 
 use Evrinoma\ContractorBundle\Validator\ContractorValidator;
+use Evrinoma\UtilsBundle\DependencyInjection\Compiler\AbstractConstraint;
 use Symfony\Component\DependencyInjection\Compiler\CompilerPassInterface;
-use Symfony\Component\DependencyInjection\ContainerBuilder;
-use Symfony\Component\DependencyInjection\Reference;
 
 /**
- * Class MenuItemPass
+ * Class ConstraintPass
  *
  * @package Evrinoma\MenuBundle\DependencyInjection
  */
-class ConstraintPass implements CompilerPassInterface
+class ConstraintPass extends AbstractConstraint implements CompilerPassInterface
 {
     public const CONTRACTOR_CONSTRAINT = 'evrinoma.contractor.constraint';
 
-    /**
-     * @inheritDoc
-     */
-    public function process(ContainerBuilder $container)
-    {
-        if (!$container->has(ContractorValidator::class)) {
-            return;
-        }
-
-        $definition = $container->findDefinition(ContractorValidator::class);
-
-        $taggedServices = $container->findTaggedServiceIds(self::CONTRACTOR_CONSTRAINT);
-
-        foreach ($taggedServices as $id => $tags) {
-            $definition->addMethodCall('addConstraint', [new Reference($id)]);
-        }
-    }
+    protected static string $alias = self::CONTRACTOR_CONSTRAINT;
+    protected static string $class = ContractorValidator::class;
+    protected static string $methodCall = 'addConstraint';
 }
