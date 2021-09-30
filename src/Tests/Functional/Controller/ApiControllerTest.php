9ecb4235fdd692831c86e41d12cb804a50adc050
Nikolay Nikolaev <evrinoma@gmail.com>
2021-09-30 15:35:42 +0300

    fix and refactoring tests

--- src/Tests/Functional/Controller/ApiControllerTest.php
+++ src/Tests/Functional/Controller/ApiControllerTest.php
@@ -6,15 +6,21 @@ namespace Evrinoma\ContractorBundle\Tests\Functional\Controller;
 use Evrinoma\ContractorBundle\Dto\ContractorApiDto;
 use Evrinoma\ContractorBundle\Fixtures\ContractorFixtures;
 use Evrinoma\ContractorBundle\Tests\Functional\CaseTest;
+use Evrinoma\TestUtilsBundle\Browser\ApiBrowserTestInterface;
+use Evrinoma\TestUtilsBundle\Browser\ApiBrowserTestTrait;
 use Evrinoma\TestUtilsBundle\Controller\ApiControllerTestInterface;
+use Evrinoma\TestUtilsBundle\Helper\ApiHelperTestInterface;
+use Evrinoma\TestUtilsBundle\Helper\ApiHelperTestTrait;
 use Evrinoma\UtilsBundle\Model\ActiveModel;
 use Symfony\Component\HttpFoundation\Response;
 
 /**
  * @group functional
  */
-class ApiControllerTest extends CaseTest implements ApiControllerTestInterface
+class ApiControllerTest extends CaseTest implements ApiControllerTestInterface, ApiBrowserTestInterface, ApiHelperTestInterface
 {
+    use ApiBrowserTestTrait, ApiHelperTestTrait;
+
 //region SECTION: Protected
     protected function loadContractorFixtures(): void
     {
@@ -26,7 +32,6 @@ class ApiControllerTest extends CaseTest implements ApiControllerTestInterface
         return ContractorApiDto::class;
     }
 
-
     protected function setDefault(): array
     {
         return [
@@ -34,62 +39,29 @@ class ApiControllerTest extends CaseTest implements ApiControllerTestInterface
             "id"         => 1,
             "active"     => "a",
             "created_at" => "2021-06-08 17:46",
-
+            "class"      => $this->getDtoClass(),
         ];
-    }
-
-    protected function queryCreateContractor(array $query): void
-    {
-        $this->client->restart();
-
-        $this->client->request('POST', 'evrinoma/api/contractor/create', [], [], ['CONTENT_TYPE' => 'application/json'], json_encode($query));
-    }
-
-
-    protected function queryDeleteContractor(array $query): void
-    {
-        $this->client->restart();
-
-        $this->client->request('DELETE', 'evrinoma/api/contractor/delete', $query);
-    }
-
-    protected function queryPutContractor(array $query): void
-    {
-        $this->client->restart();
-
-        $this->client->request('PUT', 'evrinoma/api/contractor/save', [], [], ['CONTENT_TYPE' => 'application/json'], json_encode($query));
-    }
-
-    protected function queryGetContractor(array $query): void
-    {
-        $this->client->restart();
-
-        $this->client->request('GET', 'evrinoma/api/contractor', $query);
-    }
-
-    protected function queryCriteriaContractor(array $query): void
-    {
-        $this->client->restart();
-
-        $this->client->request('GET', 'evrinoma/api/contractor/criteria', $query);
     }
 //endregion Protected
 
 //region SECTION: Public
     public function testCriteria(): void
     {
+        $query = $this->getDefault();
+
         $this->createIdentity();
         $this->assertEquals(Response::HTTP_CREATED, $this->client->getResponse()->getStatusCode());
         $this->createIdentityDependency();
         $this->assertEquals(Response::HTTP_CREATED, $this->client->getResponse()->getStatusCode());
-        $query = $this->createIdentityDependencyIsolate();
+        $this->createIdentityDependencyIsolate();
         $this->assertEquals(Response::HTTP_CREATED, $this->client->getResponse()->getStatusCode());
 
-        $response = $this->criteria(["identity" => "1234567890"]);
+        $response = $this->criteria(["class" => $this->getDtoClass(), "identity" => "1234567890"]);
         $this->assertEquals(Response::HTTP_OK, $this->client->getResponse()->getStatusCode());
         $this->assertArrayHasKey('data', $response);
         $this->assertCount(2, $response['data']);
-        $response = $this->criteria(["identity" => md5($query['name'])]);
+
+        $response = $this->criteria(["class" => $this->getDtoClass(), "identity" => md5($query['name'])]);
         $this->assertEquals(Response::HTTP_OK, $this->client->getResponse()->getStatusCode());
         $this->assertArrayHasKey('data', $response);
         $this->assertCount(1, $response['data']);
@@ -102,27 +74,32 @@ class ApiControllerTest extends CaseTest implements ApiControllerTestInterface
         $this->createIdentityDependency();
         $this->assertEquals(Response::HTTP_CREATED, $this->client->getResponse()->getStatusCode());
 
-        $response = $this->criteria(["identity" => "0987654321"]);
+        $response = $this->criteria(["class" => $this->getDtoClass(), "identity" => "0987654321"]);
         $this->assertEquals(Response::HTTP_NOT_FOUND, $this->client->getResponse()->getStatusCode());
         $this->assertArrayHasKey('data', $response);
     }
 
     public function testPut(): void
     {
-        $query = $this->createIdentityDependency();
+        $created = $this->createIdentityDependency();
         $this->assertEquals(Response::HTTP_CREATED, $this->client->getResponse()->getStatusCode());
-        $response = $this->getById(1);
+
+        $find = $this->get(1);
         $this->assertEquals(Response::HTTP_OK, $this->client->getResponse()->getStatusCode());
-        $this->assertArrayHasKey('data', $response);
+        $this->assertArrayHasKey('data', $find);
 
-        $entity              = $response['data'];
-        $query['isolate']    = $entity['isolate'];
-        $query['created_at'] = $entity['created_at'];
+        $this->assertArrayHasKey('data', $created);
+        $this->assertArrayHasKey('data', $find);
 
-        $this->assertCount(0, array_diff($entity, $query));
+        $this->assertCount(0, array_diff($created['data'], $find['data']));
 
-        $query["identity"] = "0987654321";
-        $query["active"]   = ActiveModel::BLOCKED;
+        $query = [
+            "class"    => $this->getDtoClass(),
+            "id"       => $find['data']['id'],
+            "identity" => "0987654321",
+            "active"   => ActiveModel::BLOCKED,
+            "name"     => $find['data']['name'],
+        ];
 
         $this->put($query);
         $this->assertEquals(Response::HTTP_OK, $this->client->getResponse()->getStatusCode());
@@ -152,66 +129,74 @@ class ApiControllerTest extends CaseTest implements ApiControllerTestInterface
 
         $this->put($query);
         $this->assertEquals(Response::HTTP_UNPROCESSABLE_ENTITY, $this->client->getResponse()->getStatusCode());
-    }
 
+        $created = $this->createIdentityDependency();
+        $this->assertEquals(Response::HTTP_CREATED, $this->client->getResponse()->getStatusCode());
+
+        $query = [
+            "id"       => "1",
+            "identity" => "0987654321",
+            "active"   => ActiveModel::BLOCKED,
+        ];
+
+        $this->put($query);
+        $this->assertEquals(Response::HTTP_UNPROCESSABLE_ENTITY, $this->client->getResponse()->getStatusCode());
+    }
 
     public function testDelete(): void
     {
-        $query = $this->createIdentityDependency();
+        $created = $this->createIdentityDependency();
         $this->assertEquals(Response::HTTP_CREATED, $this->client->getResponse()->getStatusCode());
-        $response = $this->getById(1);
-        $this->assertEquals(Response::HTTP_OK, $this->client->getResponse()->getStatusCode());
-        $this->assertArrayHasKey('data', $response);
 
-        $response = $this->deleteById('1');
-        $this->assertArrayHasKey('data', $response);
+        $find = $this->get(1);
+        $this->assertEquals(Response::HTTP_OK, $this->client->getResponse()->getStatusCode());
+
+        $this->assertArrayHasKey('data', $created);
+        $this->assertArrayHasKey('data', $find);
+
+        $response = $this->delete('1');
         $this->assertEquals(Response::HTTP_ACCEPTED, $this->client->getResponse()->getStatusCode());
 
-        $response = $this->getById(1);
+        $delete = $this->get(1);
+
+        $this->assertArrayHasKey('data', $delete);
         $this->assertArrayHasKey('data', $response);
 
-        $entity              = $response['data'];
-        $query['isolate']    = $entity['isolate'];
-        $query['created_at'] = $entity['created_at'];
-
-        $this->assertCount(1, array_diff($entity, $query));
-        $this->assertEquals(ActiveModel::DELETED, $entity['active']);
+        $this->assertCount(1, array_diff($find['data'], $delete['data']));
+        $this->assertEquals(ActiveModel::DELETED, $delete['data']['active']);
     }
 
     public function testDeleteNotFound(): void
     {
-        $response = $this->deleteById('1');
+        $response = $this->delete('1');
         $this->assertArrayHasKey('data', $response);
         $this->assertEquals(Response::HTTP_NOT_FOUND, $this->client->getResponse()->getStatusCode());
     }
 
     public function testDeleteUnprocessable(): void
     {
-        $response = $this->deleteById('');
+        $response = $this->delete('');
         $this->assertArrayHasKey('data', $response);
         $this->assertEquals(Response::HTTP_UNPROCESSABLE_ENTITY, $this->client->getResponse()->getStatusCode());
     }
 
-
     public function testGet(): void
     {
-        $query = $this->createIdentityDependency();
+        $created = $this->createIdentityDependency();
         $this->assertEquals(Response::HTTP_CREATED, $this->client->getResponse()->getStatusCode());
 
-        $response = $this->getById(1);
+        $find = $this->get(1);
         $this->assertEquals(Response::HTTP_OK, $this->client->getResponse()->getStatusCode());
-        $this->assertArrayHasKey('data', $response);
 
-        $entity              = $response['data'];
-        $query['isolate']    = $entity['isolate'];
-        $query['created_at'] = $entity['created_at'];
+        $this->assertArrayHasKey('data', $created);
+        $this->assertArrayHasKey('data', $find);
 
-        $this->assertCount(0, array_diff($entity, $query));
+        $this->assertCount(0, array_diff($created['data'], $find['data']));
     }
 
     public function testGetNotFound(): void
     {
-        $response = $this->getById(1);
+        $response = $this->get(1);
         $this->assertArrayHasKey('data', $response);
         $this->assertEquals(Response::HTTP_NOT_FOUND, $this->client->getResponse()->getStatusCode());
     }
@@ -256,77 +241,43 @@ class ApiControllerTest extends CaseTest implements ApiControllerTestInterface
 
     public function testPostUnprocessable(): void
     {
-        $this->createWrong();
+        $this->postWrong();
 
         $this->assertEquals(Response::HTTP_UNPROCESSABLE_ENTITY, $this->client->getResponse()->getStatusCode());
     }
 //endregion Public
 
 //region SECTION: Private
-    private function deleteById(string $id): array
-    {
-        $this->queryDeleteContractor(["class" => $this->getDtoClass(), "id" => $id,]);
-
-        return json_decode($this->client->getResponse()->getContent(), true);
-    }
-
-    private function getById(int $id): array
-    {
-        $this->queryGetContractor(["class" => $this->getDtoClass(), "id" => $id,]);
-
-        return json_decode($this->client->getResponse()->getContent(), true);
-    }
-
-    private function criteria(array $query): array
-    {
-        $this->queryCriteriaContractor(array_merge(["class" => $this->getDtoClass()], $query));
-
-        return json_decode($this->client->getResponse()->getContent(), true);
-    }
-
-
-    private function put(array $query): array
-    {
-        $this->queryPutContractor($query);
-
-        return $query;
-    }
-
-
     private function createIdentity(): array
     {
-        $query = $this->getDefault(["class" => $this->getDtoClass(), "identity" => "1234567890",]);
+        $query = $this->getDefault(["identity" => "1234567890",]);
 
-        $this->queryCreateContractor($query);
-
-        return $query;
+        return $this->post($query);
     }
 
     private function createIdentityDependency(): array
     {
-        $query = $this->getDefault(["class" => $this->getDtoClass(), "identity" => "1234567890", "dependency" => "1234567890"]);
+        $query = $this->getDefault(["identity" => "1234567890", "dependency" => "1234567890"]);
 
-        $this->queryCreateContractor($query);
-
-        return $query;
+        return $this->post($query);;
     }
 
     private function createIdentityDependencyIsolate(): array
     {
-        $query = $this->getDefault(["class" => $this->getDtoClass(),]);
+        $query = $this->getDefault();
 
-        $this->queryCreateContractor($query);
-
-        return $query;
+        return $this->post($query);
     }
 
-    private function createWrong(): array
+//region SECTION: Getters/Setters
+    public function setUp(): void
     {
-        $query = $this->getDefault([]);
-
-        $this->queryCreateContractor($query);
-
-        return $query;
+        parent::setUp();
+        $this->getUrl      = 'evrinoma/api/contractor';
+        $this->criteriaUrl = 'evrinoma/api/contractor/criteria';
+        $this->deleteUrl   = 'evrinoma/api/contractor/delete';
+        $this->putUrl      = 'evrinoma/api/contractor/save';
+        $this->postUrl     = 'evrinoma/api/contractor/create';
     }
 //endregion Getters/Setters
 }
