b1d862dfaa56869e112c854162c0cfc2723651c9
Nikolay Nikolaev <evrinoma@gmail.com>
2021-12-16 00:13:21 +0300

    fix tes value objects

--- src/Tests/Functional/ValueObject/Id.php
+++ src/Tests/Functional/ValueObject/Id.php
@@ -2,33 +2,8 @@
 
 namespace Evrinoma\ContractorBundle\Tests\Functional\ValueObject;
 
-use Evrinoma\TestUtilsBundle\ValueObject\ValueObjectTest;
+use Evrinoma\TestUtilsBundle\ValueObject\Common\AbstractId;
 
-class Id implements ValueObjectTest
+class Id extends AbstractId
 {
-//region SECTION: Fields
-    protected const VALUE = "1";
-//endregion Fields
-
-//region SECTION: Public
-    public static function value(): string
-    {
-        return static::VALUE;
-    }
-
-    public static function wrong(): string
-    {
-        return "100000";
-    }
-
-    public static function empty(): string
-    {
-        return '';
-    }
-
-    public static function nullable()
-    {
-        return null;
-    }
-//endregion Public
 }
--- src/Tests/Functional/ValueObject/Identity.php
+++ src/Tests/Functional/ValueObject/Identity.php
@@ -2,33 +2,11 @@
 
 namespace Evrinoma\ContractorBundle\Tests\Functional\ValueObject;
 
-use Evrinoma\TestUtilsBundle\ValueObject\ValueObjectTest;
+use Evrinoma\TestUtilsBundle\ValueObject\Common\AbstractIdentity;
 
-class Identity implements ValueObjectTest
+class Identity extends AbstractIdentity
 {
 //region SECTION: Fields
-    protected const VALUE = "1234567890";
+    protected static string $value = "1234567890";
 //endregion Fields
-
-//region SECTION: Public
-    public static function value(): string
-    {
-        return static::VALUE;
-    }
-
-    public static function wrong(): string
-    {
-        return strrev(static::value());
-    }
-
-    public static function empty(): string
-    {
-        return '';
-    }
-
-    public static function nullable()
-    {
-        return null;
-    }
-//endregion Public
 }
